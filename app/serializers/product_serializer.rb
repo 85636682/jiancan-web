ccec5814e14e9b37c71c053b12e275018cad1994
dongqilai <85636682@qq.com>
2015-08-25 12:50:12 +0800

    avatar

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,6 +1,10 @@
 class ProductSerializer < BaseSerializer
   attributes :id, :name, :shop, :avatar, :created_at, :updated_at
 
+  def avatar
+    object.avatar.url("80x80")
+  end
+
   def shop
     ShopSerializer.new(object.shop, root: false)
   end
--- app/serializers/shop_serializer.rb
+++ app/serializers/shop_serializer.rb
@@ -1,6 +1,10 @@
 class ShopSerializer < BaseSerializer
   attributes :id, :name, :address, :mobile, :province, :city, :district, :merchant, :avatar, :created_at, :updated_at
 
+  def avatar
+    object.avatar.url("80x80")
+  end
+
   def merchant
     MerchantSerializer.new(object.merchant, root: false)
   end
