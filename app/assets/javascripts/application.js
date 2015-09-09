1e5ac16ffa2f0e44bfe819225b55df5965a682e7
dongqilai <85636682@qq.com>
2015-09-09 09:09:31 +0800

    js修复

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,18 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
+//= require nprogress
 //= require_tree .
+//= require turbolinks
+
+NProgress.configure({
+  speed: 300,
+  minimum: 0.03,
+  ease: 'ease'
+});
+
+$(document).on('page:fetch',   function() { NProgress.start(); });
+$(document).on('page:change',  function() { NProgress.done(); });
+$(document).on('page:restore', function() { NProgress.remove(); });
--- app/assets/javascripts/cpanel.js
+++ app/assets/javascripts/cpanel.js
@@ -1,5 +1,17 @@
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap
 //= require 'china_city/jquery.china_city'
+//= require nprogress
+//= require turbolinks
+
+NProgress.configure({
+  speed: 300,
+  minimum: 0.03,
+  ease: 'ease'
+});
+
+$(document).on('page:fetch',   function() { NProgress.start(); });
+$(document).on('page:change',  function() { NProgress.done(); });
+$(document).on('page:restore', function() { NProgress.remove(); });
