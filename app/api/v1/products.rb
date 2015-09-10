19dc062783d68bfe81e2ddc8c48e8e510fb94eac
dongqilai <85636682@qq.com>
2015-09-10 11:04:03 +0800

    api get products

--- app/api/v1/products.rb
+++ app/api/v1/products.rb
@@ -1,7 +1,13 @@
 module V1
   class Products < Grape::API
     resource :products do
-      get ''
+      desc '返回某个商铺的商品'
+      params do
+        requires :shop_id, type: Integer, desc: '商铺的Id'
+      end
+      get '', serializer: ProductSerializer, root: 'products' do
+        @products = Products.where(:shop_id => params[:shop_id]).order("created_at ASC")
+      end
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823042635) do
+ActiveRecord::Schema.define(version: 20150909084543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20150823042635) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "room_id"
   end
 
   create_table "products", force: :cascade do |t|
