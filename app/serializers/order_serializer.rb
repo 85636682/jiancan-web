7ab0a0f696997638c47c5bf6f3d6ae83d6493882
dongqilai <85636682@qq.com>
2015-09-21 12:23:46 +0800

    format updated_at

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -12,4 +12,8 @@ class OrderSerializer < BaseSerializer
   def created_at
     DateTime.parse(object.created_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
   end
+
+  def updated_at
+    DateTime.parse(object.updated_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
 end
