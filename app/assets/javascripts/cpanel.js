e037ba882b2c8db863f4dc30d91bcd132b4d85c0
dongqilai <85636682@qq.com>
2015-09-21 15:08:11 +0800

    Pnotify

--- app/assets/javascripts/cpanel.js
+++ app/assets/javascripts/cpanel.js
@@ -27,12 +27,14 @@ $(document).on('page:fetch',   function() { NProgress.start(); });
 $(document).on('page:change',  function() { 
   NProgress.done();
 
+  PNotify.prototype.options.styling = "fontawesome";
+
   MessageBus.start(); // call once at startup
   // how often do you want the callback to fire in ms
   MessageBus.callbackInterval = 500;
   MessageBus.subscribe("/notifications_count/" + App.access_token, function(data){
     // data shipped from server
-    alert("start2");
+
     var div, link, new_title, span, url;
     span = $(".notification-count span");
     link = $(".notification-count a");
