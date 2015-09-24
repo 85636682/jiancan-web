b73f405e8cf2ef618c7ef4a124c88801098a7d9e
dongqilai <85636682@qq.com>
2015-09-24 21:40:22 +0800

    返回格式

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < BaseSerializer
-  attributes :id, :created_at, :updated_at, :room_id, :room, :worker_id, :worker, :products
+  attributes :id, :sn, :status, :created_at, :updated_at, :room_id, :room, :worker_id, :worker
 
   def room
     RoomSerializer.new(object.room, root: false)
@@ -15,5 +15,11 @@ class OrderSerializer < BaseSerializer
 
   def updated_at
     DateTime.parse(object.updated_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
+
+  has_many :products
+
+  def products
+    object.products
   end
 end
