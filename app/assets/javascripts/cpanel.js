c36e71eec27f388e9fc5b0091de8ae68dac57985
dongqilai <85636682@qq.com>
2015-09-28 18:30:43 +0800

    messagebus start

--- app/assets/javascripts/cpanel.js
+++ app/assets/javascripts/cpanel.js
@@ -23,11 +23,14 @@ NProgress.configure({
   ease: 'ease'
 });
 
-$(document).on('page:fetch',   function() { NProgress.start(); });
+$(document).on('page:fetch',   function() { 
+  NProgress.start();
+  MessageBus.start(); // call once at startup
+});
 $(document).on('page:change',  function() {
   NProgress.done();
 
-  MessageBus.start(); // call once at startup
+  
   // how often do you want the callback to fire in ms
   MessageBus.callbackInterval = 60000;
   MessageBus.subscribe("/notifications_count/" + App.access_token, function(data){
