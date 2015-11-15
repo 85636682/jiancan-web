ccaf18bf4f5546de8c508aa31e0dd980fc88c2ca
dongqilai <85636682@qq.com>
2015-11-15 20:45:26 +0800

    增加status.text

--- app/serializers/order_product_serializer.rb
+++ app/serializers/order_product_serializer.rb
@@ -1,6 +1,6 @@
 class OrderProductSerializer < BaseSerializer
   attributes :id, :order_id, :product_id, :product, :quantity, :status,
-             :created_at, :updated_at
+             :status_text, :created_at, :updated_at
 
   def product
     ProductSerializer.new(object.product, root: false)
@@ -12,5 +12,9 @@ class OrderProductSerializer < BaseSerializer
 
   def updated_at
     DateTime.parse(object.updated_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
+
+  def status_text
+    object.status.text
   end
 end
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < BaseSerializer
-  attributes :id, :sn, :status, :total_price, :created_at, :updated_at, :room_id, :room, :worker_id, :worker, :shop_id, :shop
+  attributes :id, :sn, :status, :status_text, :total_price, :created_at, :updated_at, :room_id, :room, :worker_id, :worker, :shop_id, :shop
 
   has_many :order_products, serializer: OrderProductSerializer
 
@@ -21,6 +21,10 @@ class OrderSerializer < BaseSerializer
 
   def updated_at
     DateTime.parse(object.updated_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
+
+  def status_text
+    object.status.text
   end
 
 end
