146187ec9261045df51197ef5624872587cbd83e
dongqilai <85636682@qq.com>
2016-01-02 00:11:11 +0800

    新版本

--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :cpanel do
-    get 'home/index'
-    get 'home/prices'
-    get 'home/about'
-    get 'home/statistic'
-    resources :shops, expect: :destroy
-    resources :products
-    resources :food_materials
-    resources :rooms
-    resources :orders do
-      member do
-        get :settle
-        get :cancel
-      end
-      collection do
-        get :search
-      end
-    end
-    resources :order_products, only: [:new, :create, :destroy] do
-      member do
-        get :status
-      end
-    end
-    resources :categories
-    resources :workers
-    resources :market_products, only: [:index, :show]
-    resources :notifications, only: [:index, :destroy] do
-      member do
-        get :read
-      end
-      collection do
-        get :read_all
-        get :clear
-      end
-    end
-    resources :cart, only: [:index, :create, :update, :destroy]
-    resources :market_orders, only: [:index, :show, :create, :destroy]
-    resources :merchants, only: [:edit, :update] do
-      collection do
-        patch 'update_password'
-      end
-    end
-  end
-
-  devise_for :merchants, controllers: {
-    sessions: "merchants/sessions"
-  }
-
   namespace :admin do
     resources :market_products
     resources :shops
