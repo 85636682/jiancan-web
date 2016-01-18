cb37ccda3989655fabd58a1a91d3cbd9dd790cea
dongqilai <85636682@qq.com>
2016-01-18 21:42:45 +0800

    微信验证

--- app/serializers/shop_public_account_serializer.rb
+++ app/serializers/shop_public_account_serializer.rb
@@ -1,3 +1,8 @@
 class ShopPublicAccountSerializer < BaseSerializer
-  attributes :id, :shop_id, :public_account, :app_id, :app_secret
+  attributes :id, :shop_id, :public_account, :app_id, :app_secret, :is_valid
+
+  def is_valid
+    @client ||= WeixinAuthorize::Client.new(self.app_id, self.app_secret)
+    @client.is_valid?
+  end
 end
