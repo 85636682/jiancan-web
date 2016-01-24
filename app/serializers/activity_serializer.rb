e90cdca39519171b82887116d98f9ba9e8d1b16e
dongqilai <85636682@qq.com>
2016-01-24 10:50:18 +0800

    商家活动

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,11 +1,15 @@
 class ActivitySerializer < BaseSerializer
   attributes :id, :title, :details, :amount, :status, :status_text, :deadline,
-             :handle, :created_at, :updated_at, :likes
+             :handle, :handle_text, :created_at, :updated_at, :likes
 
   has_many :activity_users
   has_many :activity_products
 
   def status_text
     object.status.text
+  end
+
+  def handle_text
+    object.handle.text
   end
 end
