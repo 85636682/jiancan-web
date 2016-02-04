438d925ace33665fb2a6c0ebf7ef5328c8d79b50
dongqilai <85636682@qq.com>
2016-02-04 10:53:51 +0800

    商家活动

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -32,8 +32,11 @@ class WechatsController < ApplicationController
   end
    # 调用微信授权获取openid
   def invoke_wx_auth
-    if params[:state].present? || session['openid'].present? || session[:user_id].present? #|| !is_wechat_brower?
-      return # 防止进入死循环授权
+    if params[:state].present? || session['openid'].present? #|| !is_wechat_brower?
+      #@user = User.find_by_weixin_open_id(session['openid'])
+      #if @user.present?
+        return # 防止进入死循环授权
+      #end
     end
     sns_url =  @wechat_client.authorize_url(request.url, scope="snsapi_userinfo")
     redirect_to sns_url and return
@@ -46,7 +49,7 @@ class WechatsController < ApplicationController
       sns_info = @wechat_client.get_oauth_access_token(params[:code])
       # 如果网页授权作用域为snsapi_userinfo，则此时开发者可以通过access_token和openid拉取用户信息了。
       user_info = @wechat_client.get_oauth_userinfo(sns_info.result["openid"], sns_info.result["access_token"])
-      Rails.logger.error("Weixin oauth2 response: #{user_info}")
+      Rails.logger.error("Weixin oauth2 response: #{user_info.result}")
       # @user = User.from_omniauth(sns_info.result["openid"])
       # 重复使用相同一个code调用时：
       if sns_info.result["errcode"] != "40029"
