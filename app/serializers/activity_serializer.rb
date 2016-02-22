514e862129ba7a7bbb10c72e4dbd64c1e5c92aa1
dongqilai <85636682@qq.com>
2016-02-22 10:01:06 +0800

    图标

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,9 +1,13 @@
 class ActivitySerializer < BaseSerializer
   attributes :id, :title, :details, :amount, :status, :status_text, :deadline,
-             :handle, :handle_text, :created_at, :updated_at, :likes
+             :handle, :handle_text, :created_at, :updated_at, :likes, :avatar
 
   has_many :activity_users
   has_many :activity_products
+
+  def avatar
+    object.avatar.url("80x80")
+  end
 
   def status_text
     object.status.text
--- app/serializers/shop_advertisement_serializer.rb
+++ app/serializers/shop_advertisement_serializer.rb
@@ -1,5 +1,9 @@
 class ShopAdvertisementSerializer < BaseSerializer
-  attributes :id, :title, :content, :divide_into
+  attributes :id, :title, :content, :divide_into, :avatar
 
   has_many :shop_advertisement_users
+
+  def avatar
+    object.avatar.url("80x80")
+  end
 end
