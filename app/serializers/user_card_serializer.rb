7596c600998c97a9251b67b62659c14aa108b5f1
dongqilai <85636682@qq.com>
2016-02-23 08:28:09 +0800

    user card

--- app/serializers/user_card_serializer.rb
+++ app/serializers/user_card_serializer.rb
@@ -1,6 +1,10 @@
 class UserCardSerializer < BaseSerializer
   attributes :id, :title, :color, :notice, :description, :service_phone, :sku,
-             :date_info, :shop_id, :created_at, :updated_at
+             :date_info, :shop_id, :created_at, :updated_at, :logo
 
   has_one :shop
+
+  def logo
+    object.logo.url("80x80")
+  end
 end
