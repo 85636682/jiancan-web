9f5b43566d84830c52da9575037d886caae39938
dongqilai <85636682@qq.com>
2016-02-24 15:07:21 +0800

    user card

--- app/serializers/user_card_serializer.rb
+++ app/serializers/user_card_serializer.rb
@@ -5,7 +5,7 @@ class UserCardSerializer < BaseSerializer
              :supply_balance, :balance_url, :bonus_rules, :balance_rules,
              :activate_url, :cost_money_unit, :increase_bonus, :max_increase_bonus,
              :init_increase_bonus, :cost_bonus_unit, :reduce_money,
-             :least_money_to_use_bonus, :max_reduce_bonus, :discount, :bonus_cleared，
+             :least_money_to_use_bonus, :max_reduce_bonus, :discount, :bonus_cleared,
              :balance_rules, :bonus_rules
 
   has_one :shop
