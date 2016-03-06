ba38a2acfab04de54d7562e38c4247b55e3022a8
dongqilai <85636682@qq.com>
2016-03-06 23:43:04 +0800

    去除department

--- app/api/v1/workers.rb
+++ app/api/v1/workers.rb
@@ -5,16 +5,11 @@ module V1
       params do
         requires :login, type: String, desc: "员工账号"
         requires :password,  type: String, desc: "员工密码"
-        requires :department, type: String, desc: "部门waiter||kitchen||counter"
       end
       post 'login' do
         worker = Worker.find_by_login(params[:login])
         if worker && worker.authenticate(params[:password])
-          if worker.department == params[:department]
-            { msg: "登录成功！", login: worker.login, access_token: worker.get_private_token, pusher_id: worker.pusher_id }
-          else
-            error!({ error: "用户不属于这个部门！" }, 400)
-          end
+          { msg: "登录成功！", login: worker.login, access_token: worker.get_private_token, pusher_id: worker.pusher_id }
         else
           error!({ error: "用户和密码不正确！" }, 400)
         end
