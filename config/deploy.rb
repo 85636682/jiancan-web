e122a41ee237cb21ee3f878f0d51e6ec0d9a5c56
dongqilai <85636682@qq.com>
2016-03-27 10:21:53 +0800

    微信

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,7 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
+set :linked_files, %w{config/wechat.yml}
 set :linked_dirs, %w{tmp/pids tmp/sockets log}
 
 namespace :deploy do
