188301c47c48ad7076616c64702f4885b03c7fbf
dongqilai <85636682@qq.com>
2016-04-05 13:35:20 +0800

    预定

--- app/api/u1/bookings.rb
+++ app/api/u1/bookings.rb
@@ -10,27 +10,27 @@ module U1
         authenticate!
         @bookings = current_user.bookings.where(bookingable_id: params[:bookingable_id], bookingable_type: params[:bookingable_type])
       end
-    end
 
-    desc '添加用户评论'
-    params do
-      requires :booking, type: Hash do
-        requires :shop_id, type: Integer, desc: '店铺id'
-        requires :booking_time, type: String, desc: '预定时间'
-        requires :bookingable_id, type: Integer, desc: '对象id'
-        requires :bookingable_type, type: String, desc: '对象名'
+      desc '添加用户评论'
+      params do
+        requires :booking, type: Hash do
+          requires :shop_id, type: Integer, desc: '店铺id'
+          requires :booking_time, type: String, desc: '预定时间'
+          requires :bookingable_id, type: Integer, desc: '对象id'
+          requires :bookingable_type, type: String, desc: '对象名'
+        end
       end
-    end
-    post '', serializer: BookingSerializer, root: false do
-      authenticate!
-      @booking = Booking.new(params[:booking])
-      @booking.user_id = current_user.id
-      if @booking.save
-        render @booking
-      else
-        error!({ error: "留言失败！" }, 400)
+      post '', serializer: BookingSerializer, root: false do
+        authenticate!
+        @booking = Booking.new(params[:booking])
+        @booking.user_id = current_user.id
+        if @booking.save
+          render @booking
+        else
+          error!({ error: "留言失败！" }, 400)
+        end
       end
-    end
 
+    end
   end
 end
--- app/api/u1/comments.rb
+++ app/api/u1/comments.rb
@@ -9,27 +9,27 @@ module U1
       get '', each_serializer: CommentSerializer, root: false do
         @comments = Comment.where(commentable_id: params[:commentable_id], commentable_type: params[:commentable_type])
       end
-    end
 
-    desc '添加用户评论'
-    params do
-      requires :comment, type: Hash do
-        requires :content, type: String, desc: '留言内容'
-        requires :score, type: Integer, desc: '评分'
-        requires :commentable_id, type: Integer, desc: '对象id'
-        requires :commentable_type, type: String, desc: '对象名'
+      desc '添加用户评论'
+      params do
+        requires :comment, type: Hash do
+          requires :content, type: String, desc: '留言内容'
+          requires :score, type: Integer, desc: '评分'
+          requires :commentable_id, type: Integer, desc: '对象id'
+          requires :commentable_type, type: String, desc: '对象名'
+        end
       end
-    end
-    post '', serializer: CommentSerializer, root: false do
-      authenticate!
-      @comment = Comment.new(params[:comment])
-      @comment.user_id = current_user.id
-      if @comment.save
-        render @comment
-      else
-        error!({ error: "留言失败！" }, 400)
+      post '', serializer: CommentSerializer, root: false do
+        authenticate!
+        @comment = Comment.new(params[:comment])
+        @comment.user_id = current_user.id
+        if @comment.save
+          render @comment
+        else
+          error!({ error: "留言失败！" }, 400)
+        end
       end
+      
     end
-
   end
 end
