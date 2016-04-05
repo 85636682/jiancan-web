9d721f7671899dc9041a4d0d0566250b4f7613ef
dongqilai <85636682@qq.com>
2016-04-05 22:00:35 +0800

    预定

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,6 +1,15 @@
 class BookingSerializer < BaseSerializer
-  attributes :id, :shop_id, :user_id, :bookingable_id, :bookingable_type
+  attributes :id, :shop_id, :user_id, :bookingable_id, :bookingable_type,
+             :booking_time, :confirmed, :confirmed_text
 
   has_one :shop
   has_one :bookingable, :polymorphic => true
+
+  def booking_time
+    DateTime.parse(object.booking_time.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
+
+  def confirmed_text
+    confirmed ? "等待确认" : "已确认"
+  end
 end
