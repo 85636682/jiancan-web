9e8e16dcbf556d6040efa74ceb707d84d8ad675c
dongqilai <85636682@qq.com>
2016-04-18 21:59:09 +0800

    支付测试

--- app/api/u1/pay.rb
+++ app/api/u1/pay.rb
@@ -38,7 +38,7 @@ module U1
           package = "prepay_id=#{r["prepay_id"]}"
           JcLog.create(:content => package)
           paySign = Digest::MD5.hexdigest("appId=#{r["appid"]}&timeStamp=#{Time.now.to_i.to_s}&nonceStr=#{SecureRandom.uuid.tr('-', '')}&package=#{package}&signType=MD5").upcase
-          JcLog.create({ "prepay_id" => r["prepay_id"], "nonceStr" => "#{SecureRandom.uuid.tr('-', '')}", "paySign" => "#{paySign}", "return_code" => r["return_code"], "return_msg" => r["return_msg"] })
+          JcLog.create(:content => { "prepay_id" => r["prepay_id"], "nonceStr" => "#{SecureRandom.uuid.tr('-', '')}", "paySign" => "#{paySign}", "return_code" => r["return_code"], "return_msg" => r["return_msg"] })
           { "prepay_id" => r["prepay_id"], "nonceStr" => "#{SecureRandom.uuid.tr('-', '')}", "paySign" => "#{paySign}", "return_code" => r["return_code"], "return_msg" => r["return_msg"] }
         else
           { "return_code" => "FAIL", "return_msg" => r["return_msg"] }
