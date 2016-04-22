d1708c80024f695ff6c468673e2306572b7d768b
dongqilai <85636682@qq.com>
2016-04-22 16:56:37 +0800

    商品评价

--- app/api/u1/order_products.rb
+++ app/api/u1/order_products.rb
@@ -1,12 +1,22 @@
 module U1
   class OrderProducts < Grape::API
     resource :order_products do
+
       params do
       end
       get '', each_serializer: OrderProductSerializer, root: false do
         authenticate!
         OrderProduct.joins(:order).where('orders.user_id' => current_user.id).order("created_at DESC")
       end
+
+      params do
+        requires :order_product_id, type: Integer, desc: "order_product_id"
+      end
+      get 'one', serializer: OrderProductSerializer, root: false do
+        authenticate!
+        OrderProduct.find_by_id(params[:order_product_id])
+      end
+
     end
   end
 end
