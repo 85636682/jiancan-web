998c6d7b9823318cdf669aa9b5d8bb3a02df87b8
dongqilai <85636682@qq.com>
2016-04-24 19:52:51 +0800

    设置默认

--- app/serializers/receiving_address_serializer.rb
+++ app/serializers/receiving_address_serializer.rb
@@ -1,3 +1,4 @@
 class ReceivingAddressSerializer < BaseSerializer
-  attributes :id, :consignee, :mobile, :province, :city, :district, :street
+  attributes :id, :consignee, :mobile, :province, :city, :district, :street,
+             :defaulted
 end
