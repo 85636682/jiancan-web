51679cba25fd1ae76b6d0e35a654297f6d0b5e45
dongqilai <85636682@qq.com>
2016-04-25 13:13:21 +0800

    商品评论

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,13 @@
 class CommentSerializer < BaseSerializer
   attributes :id, :content, :user_id, :commentable_id, :commentable_type
+  
+  belongs_to :user
+
+  def created_at
+    DateTime.parse(object.created_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
+
+  def updated_at
+    DateTime.parse(object.updated_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
 end
