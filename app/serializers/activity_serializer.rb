20773f0deaea2eff7e4f6abbb025c71e0a5554a3
dongqilai <85636682@qq.com>
2016-04-26 17:30:15 +0800

    活动

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -17,4 +17,12 @@ class ActivitySerializer < BaseSerializer
   def handle_text
     object.handle.text
   end
+
+  def created_at
+    DateTime.parse(object.created_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
+
+  def updated_at
+    DateTime.parse(object.updated_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
 end
