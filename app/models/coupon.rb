dc7e2eb2cf2c9091228ceebb320ae2c6e4123009
dongqilai <85636682@qq.com>
2016-04-27 12:32:38 +0800

    优惠劵

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,4 +1,7 @@
 class Coupon < ActiveRecord::Base
+  mount_uploader :avatar, ImageUploader
+  attr_accessor :uploader_secure_token
+  
   belongs_to :shop
   has_many :coupon_users, :dependent => :destroy
   has_many :users, :through => :coupon_users
