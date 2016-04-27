8558f683a1dbfc31355b5ff7bfe7a91f2f2d50dc
dongqilai <85636682@qq.com>
2016-04-27 11:43:49 +0800

    优惠劵

--- app/serializers/coupon_serializer.rb
+++ app/serializers/coupon_serializer.rb
@@ -1,9 +1,17 @@
 class CouponSerializer < BaseSerializer
-  attributes :id, :shop_id, :title, :details, :avatar, :original_price,
+  attributes :id, :shop_id, :title, :details, :avatar, :avatar80x80, :original_price,
              :preferential_price, :created_at, :updated_at, :coupon_users_count
 
   has_many :coupon_users, serializer: CouponUserSerializer
   has_one :shop, serializer: ShopSerializer
+
+  def avatar
+    object.avatar.url("320xAuto")
+  end
+
+  def avatar80x80
+    object.avatar.url("80x80")
+  end
 
   def created_at
     DateTime.parse(object.created_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
