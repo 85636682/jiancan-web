bef7235d7f5a2004abfa87def505f847e74d5b6f
dongqilai <85636682@qq.com>
2016-04-27 11:27:53 +0800

    优惠劵

---
+++ app/api/u1/coupons.rb
@@ -0,0 +1,18 @@
+module U1
+  class Coupons < Grape::API
+    resource :coupons do
+
+      desc '返回店铺下所有会员卡'
+      params do
+        optional :offset, type: Integer, default: 0
+        optional :limit,  type: Integer, default: 20, values: 1..150
+      end
+      get '', each_serializer: CouponSerializer, root: false do
+        authenticate!
+        @coupons = Coupon.all.offset(params[:offset]).limit(params[:limit]).order("coupon_users_count DESC")
+        render @coupons
+      end
+
+    end
+  end
+end
