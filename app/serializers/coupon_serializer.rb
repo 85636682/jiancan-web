7ca8c4a00c6fdc0920896954252164fa4a85b51e
dongqilai <85636682@qq.com>
2016-04-27 09:05:52 +0800

    优惠劵

---
+++ app/serializers/coupon_serializer.rb
@@ -0,0 +1,14 @@
+class CouponSerializer < BaseSerializer
+  attributes :id, :title, :user_id, :details, :avatar, :original_price,
+             :preferential_price, :created_at, :updated_at
+
+  has_many :coupon_users, serializer: CouponUserSerializer
+
+  def created_at
+    DateTime.parse(object.created_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
+
+  def updated_at
+    DateTime.parse(object.updated_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
+end
---
+++ app/serializers/coupon_user_serializer.rb
@@ -0,0 +1,19 @@
+class CouponUserSerializer < BaseSerializer
+  attributes :id, :coupon_id, :user_id, :used,
+             :used_at, :created_at, :updated_at
+
+  has_one :coupon, serializer: CouponUserSerializer
+  has_one :user, serializer: UserSerializer
+
+  def used_at
+    DateTime.parse(object.used_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
+
+  def created_at
+    DateTime.parse(object.created_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
+
+  def updated_at
+    DateTime.parse(object.updated_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
+end
