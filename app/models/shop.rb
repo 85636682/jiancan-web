16d8bc0b6add50af84d76c8855b78ea47e2cb513
dongqilai <85636682@qq.com>
2016-05-08 11:53:49 +0800

    定位

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -19,12 +19,21 @@ class Shop < ActiveRecord::Base
 
   validates_uniqueness_of :name, :message => "你的店铺名称重复了"
 
-  #after_save :update_location
+  after_save :update_location
   def update_location
     return if address == ""
-    location_info = BaiduMap.address_geocoding_location(address)
+    location_info = address_geocoding_location(address)
     JcLog.create(:content => location_info)
-    #update_attributes(lat: location_info[""], lng: location_info[""], location: "")
+    update_attributes(lat: location_info[""], lng: location_info[""], location: "")
+  end
+
+  def address_geocoding_location(address: "", output: "json", options = {})
+    conn = Faraday.new(:url => "http://api.map.baidu.com") do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+    response = conn.get '/geocoder/v2/', { address: address, ak: Setting.baidu_map_ak, output: output }.merge(options)
   end
 
   def address
