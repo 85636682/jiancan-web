d69b11733a411b7d7aeee5d5c2fa9eea33a0f2d6
dongqilai <85636682@qq.com>
2016-05-18 10:15:06 +0800

    支付二维码接口

--- app/api/v1/pay.rb
+++ app/api/v1/pay.rb
@@ -3,32 +3,29 @@ module V1
     resource :pay do
 
       params do
-        requires :platform, type: String, desc: '平台名称'
-        requires :trade_type, type: String, desc: '支付方式'
+        requires :platform, type: String, desc: '平台名称，微信=wechat，支付宝=alipay'
         requires :order_id, type: Integer, desc: '支付订单id'
       end
       get '' do
-        authenticate!
         @order = Order.find_by_id(params[:order_id])
         error!({ error: "订单不存在！" }, 400) if @order.blank?
+        error!({ error: "订单未结算！"}, 400) if not @order.status.settled?
         error!({ error: "订单已完成支付！"}, 400) if @order.status.completed?
         if params[:platform] == 'wechat'
           fields = {
-            appid: '',
-            mch_id: '',
-            sub_appid: '',
-            sub_mch_id: '',
-            body: "在#{@order.shop.name}消费了#{@order.total_price}元",
+            body: "在#{@order.shop.name}消费了#{@order.total_fee}元",
             out_trade_no: @order.sn,
-            total_fee: @order.total_price,
+            total_fee: (@order.total_fee * 100).to_i,
             spbill_create_ip: '120.55.164.64',
-            notify_url: 'http://www.jiancan.me/weixin/pay_notify',
+            notify_url: 'http://www.jiancan.me/pay_notify',
             trade_type: 'NATIVE'
           }
-          r = WechatPay::Service.invoke_unifiedorder fields
-          error!({ error: "网络出错，请联系管理员！"}, 400) if r == nil
-          error!({ error: "支付出错，请联系管理员！"}, 400) unless r.success?
-          { msg: 'ok', code_url: r["code_url"] }
+          r = WxPay::Service.invoke_unifiedorder fields
+          if r.success?
+            { msg: 'ok', code_url: r["code_url"] }
+          else
+            error!({ error: "支付出错，请联系管理员！"}, 400)
+          end
         end
       end
     end
