f6292b3a12d8de73d4c0f45b7572980f995ffb60
dongqilai <85636682@qq.com>
2016-05-18 11:00:08 +0800

    优惠券

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,7 +1,7 @@
 class OrderSerializer < BaseSerializer
   attributes :id, :sn, :status, :status_text, :total_price, :created_at, :updated_at,
              :room_id, :worker_id, :shop_id, :takeout, :address, :meal_time, :pay_method,
-             :remarks, :pay_method_text, :mobile
+             :remarks, :pay_method_text, :mobile, :use_coupon
 
   def created_at
     DateTime.parse(object.created_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
@@ -9,6 +9,10 @@ class OrderSerializer < BaseSerializer
 
   def updated_at
     DateTime.parse(object.updated_at.iso8601).strftime('%Y年%m月%d日 %H:%M')
+  end
+
+  def use_coupon
+    false
   end
 
   def meal_time
