c828c06a41f58e529a9312b082a24d3414fead7f
dongqilai <85636682@qq.com>
2016-05-19 11:54:16 +0800

    app down

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( welcome.css welcome.js admin.css admin.js weui-rails.css wechat.js downapp.css weui.min.css )
+Rails.application.config.assets.precompile += %w( welcome.css welcome.js admin.css admin.js weui-rails.css wechat.js downapp.scss weui.min.css )
