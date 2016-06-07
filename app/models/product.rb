6a091f791279d493a66ad80bbb92b09f4b699ee6
dongqilai <85636682@qq.com>
2016-06-07 13:18:50 +0800

    remove ActivityProduct

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,6 @@ class Product < ActiveRecord::Base
   has_many :order_products
   has_many :orders, :through => :order_products, :dependent => :destroy
   has_many :comments, :through => :order_products, :dependent => :destroy
-  has_many :activity_products
-  has_many :activities, :through => :activity_products, :dependent => :destroy
 
   mount_uploader :avatar, ImageUploader
   attr_accessor :uploader_secure_token
